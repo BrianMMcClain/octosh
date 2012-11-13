3cbf2e715c661119d4ab3313843284e3a688970d
Brian McClain <brianmmcclain@gmail.com>
2012-11-12 22:59:39 -0500

    Updated options being stored from a hash to an OpenStruct

--- lib/octosh/cli.rb
+++ lib/octosh/cli.rb
@@ -1,26 +1,27 @@
 require 'optparse'
+require 'ostruct'
 
 module Octosh
   class CLI
     def self.start
-      options = {}
+      options = OpenStruct.new
 
       optparse = OptionParser.new do|opts|
         opts.banner = "Usage: octosh [options]"
         
         opts.on( '-s', '--script', 'Run a bash script on every host') do | script |
-          options[:script] = script
+          options.script = script
           
-          if options[:script] and options[:command]
+          if options.script and options.command
             puts "Error: Cannot specify both a script and a command to run"
             exit
           end
         end
         
         opts.on( '-c', '--comand', 'Command to run on every host' ) do | command |
-          options[:command] = command
+          options.command = command
           
-          if options[:script] and options[:command]
+          if options.script and options.command
             puts "Error: Cannot specify both a script and a command to run"
             exit
           end

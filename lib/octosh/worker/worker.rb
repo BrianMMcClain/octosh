3c04e015fa93173ae48da227c02ba0efad88c2e3
Brian McClain <brianmmcclain@gmail.com>
2012-11-12 23:56:56 -0500

    Added basic exec method

--- lib/octosh/worker/worker.rb
+++ lib/octosh/worker/worker.rb
@@ -1,12 +1,20 @@
 module Octosh
   class Worker
     
-    attr_reader :host, :user
+    attr_reader :host, :user, :password
     
-    def initalize(host, user)
+    def initalize(host, user, pass)
       @host = host
       @user = user
+      @password = pass
     end
-  
+    
+    def exec(command)
+      output = ""
+      Net::SSH.start(@host, @user, :password => @password) do |ssh|
+        output = ssh.exec!(command)
+      end
+      
+      return output
   end
 end

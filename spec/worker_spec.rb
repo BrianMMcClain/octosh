17b82db0c25ec2a8ed2e379fe5721e1b9471ec06
Brian McClain <brianmmcclain@gmail.com>
2012-11-19 20:37:54 -0500

    Added tests for Worker class

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -11,4 +11,25 @@ describe Octosh::Worker do
     worker.password.should == 'password'
   end
   
+  it "should #connect_if_not_connected" do
+    worker = Octosh::Worker.new('127.0.0.1', 'bob', 'password')
+    
+    Net::SSH.should_receive(:start).with("127.0.0.1", "bob", :password => "password", :forward_agent=>false)
+    worker.connect_if_not_connected
+  end
+  
+  it "should #exec a command" do
+    worker = Octosh::Worker.new('127.0.0.1', 'bob', 'password')
+    worker.should_receive(:exec)
+    worker.exec("date")
+  end
+  
+  it "should #exec_script" do
+    worker = Octosh::Worker.new('127.0.0.1', 'bob', 'password')
+    worker.should_receive(:put).with("/tmp/somescript.sh", kind_of(String))
+    worker.should_receive(:exec).with(kind_of(String))
+    worker.should_receive(:exec).with(kind_of(String))
+    
+    worker.exec_script("/tmp/somescript.sh")
+  end
 end

8c31f87ea4adfffc6a0a53cdc6e67c67b470ad6c
Brian McClain <brianmmcclain@gmail.com>
2012-11-19 20:23:26 -0500

    Readded initial (simple) spec test to test Travis builds

--- spec/worker_spec.rb
+++ spec/worker_spec.rb
@@ -3,13 +3,12 @@ require 'spec_helper'
 describe Octosh::Worker do
   
   it "should instantiate" do
-    # Removed until we setup mocking for the SSH communication
-    #worker = Octosh::Worker.new('127.0.0.1', 'bob', 'password')
+    worker = Octosh::Worker.new('127.0.0.1', 'bob', 'password')
     
-    #worker.should_not be_nil
-    #worker.host.should == '127.0.0.1'
-    #worker.user.should == 'bob'
-    #worker.password.should == 'password'
+    worker.should_not be_nil
+    worker.host.should == '127.0.0.1'
+    worker.user.should == 'bob'
+    worker.password.should == 'password'
   end
   
 end
